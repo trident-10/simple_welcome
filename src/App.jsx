6480b80ff7ec94051b574a3510762451a4c4f0e0
Hassan Javaid <scuderia.hj@gmail.com>
2024-07-21 10:20:32 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>Welcome to your Vite + React app.</p>
+    </div>
+  );
+}
+
+export default App;
